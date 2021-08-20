b853b82056ded0b9cc449b8ed89bad296a8e6701
DESKTOP-G7UTADU\xk_20 <xkisxk@gmail.com>
2021-08-21 00:12:21 +0800

    Added echo feature. Changed Duke's name to Alex and changed some text.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,16 +1,23 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String line = "______________________________________________";
-//        String logo = " ____        _        \n"
-//                + "|  _ \\ _   _| | _____ \n"
-//                + "| | | | | | | |/ / _ \\\n"
-//                + "| |_| | |_| |   <  __/\n"
-//                + "|____/ \\__,_|_|\\_\\___|\n";
-//        System.out.println("Hello from\n" + logo);
+        String line = "______________________________________________\n";
+        System.out.println(line + "........ Oh, sorry! I didn't see you there. I'm Alex\n" + "How may I help you?");
+        System.out.print(line);
 
-        System.out.println(line + "\nHello! I'm Duke\n" + "What can I do for you?");
-        System.out.println(line);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(line);
+        boolean isChatting = true;
+        while (isChatting)  {
+            Scanner in = new Scanner(System.in);
+            String input = in.nextLine();
+            if (input.equalsIgnoreCase("bye"))  {
+                isChatting = false;
+            } else {
+                System.out.print(line + input + "\n" + line);
+            }
+        }
+
+        System.out.println(line + "Bye. Talk to you later!");
+        System.out.print(line);
     }
 }
