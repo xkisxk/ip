932f07a55e60d2addee6da7de21334d1a090cba0
DESKTOP-G7UTADU\xk_20 <xkisxk@gmail.com>
2021-08-21 00:51:47 +0800

    Added adding and listing function.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,21 +3,33 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         String line = "______________________________________________\n";
-        System.out.println(line + "........ Oh, sorry! I didn't see you there. I'm Alex\n" + "How may I help you?");
+        System.out.println(line + "...... Oh, sorry! I didn't see you there.\nI'm Alex. How may I help you?");
         System.out.print(line);
 
+        String[] list = new String[100];
+        int listIndex = 0;
+
         boolean isChatting = true;
-        while (isChatting)  {
+
+        while (isChatting) {
             Scanner in = new Scanner(System.in);
             String input = in.nextLine();
-            if (input.equalsIgnoreCase("bye"))  {
+
+            if (input.equalsIgnoreCase("bye")) {
                 isChatting = false;
+            } else if (input.equalsIgnoreCase("list")) {
+                System.out.print(line);
+                for (int i = 0; i < listIndex; i++) {
+                    System.out.println(i+1 + ". " + list[i]);
+                }
+                System.out.print(line);
             } else {
-                System.out.print(line + input + "\n" + line);
+                list[listIndex] = input;
+                listIndex++;
+                System.out.print(line + "Added: \"" + input + "\" into the list\n" + line);
             }
         }
 
-        System.out.println(line + "Bye. Talk to you later!");
-        System.out.print(line);
+        System.out.print(line + "Bye. Talk to you later!\n" + line);
     }
 }
