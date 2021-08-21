e0565145cab1bbc4c8593ea6d9eccafac1216a50
DESKTOP-G7UTADU\xk_20 <xkisxk@gmail.com>
2021-08-21 14:56:16 +0800

    Added a Task class. Added mark as done function and updated list to display check mark.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,8 +6,9 @@ public class Duke {
         System.out.println(line + "...... Oh, sorry! I didn't see you there.\nI'm Alex. How may I help you?");
         System.out.print(line);
 
-        String[] list = new String[100];
+        int listSize = 100;
         int listIndex = 0;
+        Task[] toDoList = new Task[listSize];
 
         boolean isChatting = true;
 
@@ -15,21 +16,38 @@ public class Duke {
             Scanner in = new Scanner(System.in);
             String input = in.nextLine();
 
+            System.out.print(line);
             if (input.equalsIgnoreCase("bye")) {
                 isChatting = false;
+                System.out.println("Bye. Talk to you later!");
             } else if (input.equalsIgnoreCase("list")) {
-                System.out.print(line);
+                System.out.println("Here are the tasks in your to do list:");
                 for (int i = 0; i < listIndex; i++) {
-                    System.out.println(i+1 + ". " + list[i]);
+                    String item = i + 1 + ".[" + toDoList[i].getStatusIcon() + "] " + toDoList[i].getDescription();
+                    System.out.println(item);
                 }
-                System.out.print(line);
+            } else if (input.toLowerCase().contains("done")) {
+                String[] words = input.split("\\s+");
+                int index = Integer.parseInt(words[1]);
+
+                if (index <= listIndex) {
+                    toDoList[index - 1].markDone();
+                    System.out.println("Good job on completing this task!\nI've marked this task as done:");
+                    System.out.println("[X] " + toDoList[index - 1].getDescription());
+                } else {
+                    System.out.println("There is no item on that index.");
+                }
             } else {
-                list[listIndex] = input;
-                listIndex++;
-                System.out.print(line + "Added: \"" + input + "\" into the list\n" + line);
+                if (listIndex < listSize) {
+                    toDoList[listIndex] = new Task(input);
+                    listIndex++;
+                    System.out.println("Added: \"" + input + "\" into the list");
+                } else {
+                    System.out.println("There's too much stuff in the to do list, I can't remember them all.");
+                }
             }
+            System.out.print(line);
         }
 
-        System.out.print(line + "Bye. Talk to you later!\n" + line);
     }
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,26 @@
+public class Task {
+    private String description;
+    private boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " ");
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean getDone() {
+        return isDone;
+    }
+
+    public void markDone() {
+        isDone = true;
+    }
+
+}
