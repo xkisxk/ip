f0ad61df20416ed691299921df28304332594894
DESKTOP-G7UTADU\xk_20 <xkisxk@gmail.com>
2021-09-10 21:25:34 +0800

    Changed the taskList to an ArrayList

--- src/main/java/duke/command/TaskManager.java
+++ src/main/java/duke/command/TaskManager.java
@@ -3,6 +3,7 @@ package duke.command;
 import duke.Duke;
 import duke.task.Deadline;
 import duke.task.Event;
+import duke.task.Task;
 import duke.task.ToDo;
 
 public class TaskManager extends Duke {
@@ -53,26 +54,26 @@ public class TaskManager extends Duke {
 
     /**
      * Prints out the entire task list as a numbered list.
-     *
      */
     private void printTaskList() {
         System.out.println("Here are the tasks in your to do list:");
-        for (int i = 0; i < listIndex; i++) {
-            String item = (i + 1) + "." + taskList[i].toString();
+        int index = 1;
+        for (Task task : taskList) {
+            String item = index + "." + task.toString();
             System.out.println(item);
+            index++;
         }
     }
 
     /**
      * Marks a task as done, so the task will be tagged with a [X]
-     *
      */
     private void handleDone() {
         try {
             int index = Integer.parseInt(description);
-            taskList[index - 1].markDone();
+            taskList.get(index - 1).markDone();
             System.out.println("Good job on completing this task!\nI've marked this task as done:");
-            System.out.println(taskList[index - 1].toString());
+            System.out.println(taskList.get(index - 1).toString());
         } catch (NullPointerException e) {
             System.out.println("There is no item at that index. You have " + listIndex + " items");
         } catch (NumberFormatException e) {
@@ -87,7 +88,7 @@ public class TaskManager extends Duke {
      */
     private void handleDeadline() throws DukeException {
         try {
-            taskList[listIndex] = new Deadline(description, date);
+            taskList.add(new Deadline(description, date));
             listIndex++;
             printAddedMessage();
         } catch (ArrayIndexOutOfBoundsException e) {
@@ -102,7 +103,7 @@ public class TaskManager extends Duke {
      */
     private void handleEvent() throws DukeException {
         try {
-            taskList[listIndex] = new Event(description, date);
+            taskList.add(new Event(description, date));
             listIndex++;
             printAddedMessage();
         } catch (ArrayIndexOutOfBoundsException e) {
@@ -117,7 +118,7 @@ public class TaskManager extends Duke {
      */
     private void handleToDo() throws DukeException {
         try {
-            taskList[listIndex] = new ToDo(description);
+            taskList.add(new ToDo(description));
             listIndex++;
             printAddedMessage();
         } catch (ArrayIndexOutOfBoundsException e) {
@@ -136,7 +137,7 @@ public class TaskManager extends Duke {
      * Prints this message when a task has been successfully added.
      */
     private void printAddedMessage() {
-        System.out.println("Got it. I have added this task:\n   " + taskList[listIndex - 1]);
+        System.out.println("Got it. I have added this task:\n   " + taskList.get(listIndex - 1));
         System.out.println("Now you have " + listIndex + " tasks in the list");
     }
 
